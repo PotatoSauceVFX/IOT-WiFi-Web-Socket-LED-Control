c991359fd36df4e9612c08cbaacb51a5d8e1bbeb
RJ Nader <rj@potatosaucevfx.com>
2019-06-15 12:10:55 -0400

    Cleaned up firmware.

--- ESP32/Firmware/Firmware.ino
+++ ESP32/Firmware/Firmware.ino
@@ -5,33 +5,26 @@
 #include <WiFi.h>
 #include <WebSocketClient.h>
 
+#define PIN_STATUS_LED 2
 #define LED_DATA 13
 #define LED_COUNT 300
 
 bool ConnectToClient();
+
+char wifiSSID[] = "Pluto";
+char wifiPASSWORD[] = "10200330";
 
 char path[] = "/notifications";
 char host[] = "192.168.137.1";
 char websocketHost[] = "192.168.137.1:3000";
 int port = 3000;
 
-struct config_t
-{
-  char ssid[64];
-  char password[64];
-  byte R;
-  byte G;
-  byte B;
-  bool rst;
-} config;
-#include "EEPROM.h"
-
-
 WebSocketClient webSocketClient;
 
 CRGB leds[LED_COUNT];
 CRGB target[LED_COUNT];
-bool netLED = true;
+
+bool STATUS_LED = true;
 
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
@@ -39,59 +32,32 @@ WiFiClient client;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(2, OUTPUT);
-
-  EEPROM_initialize(256);
-  EEPROM_readAnything(0, config);
-
-  if (!config.rst) {
-    strcpy(config.ssid, "Pluto");
-    strcpy(config.password, "10200330");
-    config.R = 0;
-    config.G = 0;
-    config.B = 0;
-    config.rst = true;
-
-    EEPROM_writeAnything(0, config);
-    Serial << "Resetting EEPROM to Defaults" << endl;
-  }
+  pinMode(PIN_STATUS_LED, OUTPUT);
 
   FastLED.addLeds<WS2812B, LED_DATA, GRB>(leds, LED_COUNT);
-  leds[0].r = config.R;
-  leds[0].g = config.G;
-  leds[0].b = config.B;
-  target[0].r = config.R;
-  target[0].g = config.G;
-  target[0].b = config.B;
-  FastLED.show();
 
-  Serial << "Setting LED to last saved config: " << config.R << " " << config.G << " " << config.B << endl;
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(config.ssid);
+  Serial << "\n\nConnecting to " << wifiSSID << endl;
 
   WiFi.disconnect();
   WiFi.enableAP(false);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(config.ssid, config.password);
-
+  WiFi.begin(wifiSSID, wifiPASSWORD);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    digitalWrite(2, netLED);
-    netLED = !netLED;
+    digitalWrite(PIN_STATUS_LED, STATUS_LED);
+    STATUS_LED = !STATUS_LED;
   }
 
-  netLED = true;
-  digitalWrite(2, netLED);
+  STATUS_LED = true;
+  digitalWrite(PIN_STATUS_LED, STATUS_LED);
 
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  Serial.println("Gateway IP address: ");
+  Serial.print("Gateway IP address: ");
   Serial.println(WiFi.gatewayIP());
 }
 
@@ -99,7 +65,7 @@ long ledFadeDelay = 10;
 void loop() {
   String data;
 
-  digitalWrite(2, client.connected());
+  digitalWrite(PIN_STATUS_LED, client.connected());
 
   if (client.connected()) {
     webSocketClient.getData(data);
